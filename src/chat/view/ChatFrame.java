af48c7d2e4b6d9f80500cc74fc20ddbf01678e97
Yael_Amaya <yiap2610@gmail.com>
2017-11-13 13:45:43 -0700

    started setting up a frame and panel for the chatbot to make things look nicer on a different branch

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,23 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Mad Calvin");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 	
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -3,4 +3,18 @@ package chat.view;
 public class ChatPanel
 {
 
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
