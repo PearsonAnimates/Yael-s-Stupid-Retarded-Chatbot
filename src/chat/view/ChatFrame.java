1e048f39fc675ec12e8f6bc4f9ca23644e4af734
Yael_Amaya <yiap2610@gmail.com>
2017-11-15 12:23:29 -0700

    Reorganized and commented out some useless code (only temporarily useless b/c they're errors atm)

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -6,21 +6,21 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatbotController appController;
-	
+	private ChatPanel appPanel;
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
-		appPane = new ChatPanel(appController);
+		appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setContentPane(appPane);
+		this.setContentPane(appPanel);
+		this.setSize(600, 600);
 		this.setTitle("Mad Calvin");
 		this.setResizable(false);
-		this.setSize(600, 600);
 		this.setVisible(true);
 	}
 	
