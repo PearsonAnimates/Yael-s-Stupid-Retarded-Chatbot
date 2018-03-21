08386913a1ac3c15139f6ab9d40cbd6713331a3f
Yael_Amaya <yiap2610@gmail.com>
2018-03-21 12:57:22 -0600

    changed up CTECTwitter.java

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -127,6 +127,38 @@ public class CTECTwitter
 		}
 	}
 	
+	public String analyzeTwitterForTopic(String topic)
+	{
+		String results = "";
+		searchedTweets.clear();
+		Query twitterQuery = new Query(topic);
+		int resultMax = 750;
+		long lastId = Long.MAX_VALUE;
+		twitterQuery.setGeoCode(new GeoLocation(latitude), radius, Query.MEASUREMENT);
+		ArrayList<Status> matchingTweets = new ArrayList<Status>();
+		while(searchedTweets.size() < resultMax)
+		{
+			try
+			{
+				QueryResult resultingTweets = chatbotTwitter.search(twitterQuery);
+			}
+			catch(TwitterException error)
+			{
+				appController.handleErrors(error);
+			}
+			
+			twitterQuery.setMaxId(lastId - 1);
+		}
+		
+		results += "Talk about the search results";
+		results += "Find a tweet that will pass one of the checkers in chatbot";
+		
+		int randomTweet = (int) (Math.random() * matchingTweets.size());
+		results += matchingTweets.get(randomTweet);
+				
+		return results;
+	}
+	
 	private String removePunctuation(String currentString)
 	{
 		String punctuation = ".,'?!:;\"(){}^[]<>-";
@@ -150,6 +182,38 @@ public class CTECTwitter
 		return entries;
 	}
 	
+	private String sortedWords()
+	{
+		String allWords = "";
+		String [] words = new String [wordsAndCount.size()];
+		ArrayList<String> wordList = new ArrayList<String>(wordsAndCount.keySet());
+		
+		for(int index = 0; index < wordsAndCount.size(); index++)
+		{
+			words[index] = wordList.get(index);
+		}
+		for(int index = 0; index < words.length - 1; index++)
+		{
+			int maxIndex = index;
+			
+			for (int inner = index + 1; inner < words.length; inner++)
+			{
+				maxIndex = inner;
+			}
+			
+			String tempMax = words[maxIndex];
+			words[maxIndex] = words[index];
+			words[index] = tempMax;
+		}
+		
+		for (String word : words)
+		{
+			allWords += word + ", ";
+		}
+		
+		return allWords;
+	}
+	
 	public String getMostCommonWord(String username)
 	{
 		String mostCommon = "";
@@ -170,6 +234,8 @@ public class CTECTwitter
 		maxWord = sorted.get(0).getValue();
 		
 		mostCommon = "The most common word in " + username + "'s"+ searchedTweets.size() + " tweets is " + mostCommonWord + ", and it was used " + maxWord + "times.\nThis is " + (DecimalFormat.getPercentInstance().format(((double)maxWord)/totalWordCount)) + " of total words: :" + totalWordCount + " and is " + (DecimalFormat.getPercentInstance().format(((double)maxWord)/wordsAndCount.size())) + " of the unique words: " + wordsAndCount.size();
+		
+		mostCommon += "\n\n" + sortedWords();
 		
 		return mostCommon;
 	}
